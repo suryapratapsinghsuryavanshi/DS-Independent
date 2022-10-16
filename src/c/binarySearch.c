98f548282543c0f43617e3a09875fa3431bd0912
Faiz Khan <72645960+FaizFk@users.noreply.github.com>
2022-10-16 17:32:58 +0530

    Create binarySearch file

---
+++ src/c/binarySearch.c
@@ -0,0 +1,33 @@
+// Binary Search in C
+// Binary Search use a Sorted array and finds element in O(logn) time.
+
+#include <stdio.h>
+
+int binarySearch(int array[], int x, int low, int high) {
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main() {
+  int array[] = {3, 4, 5, 6, 7, 8, 9};  //A Sorted Array
+  int n = 7;
+  int x = 5; //Number to find
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+  return 0;
+}
