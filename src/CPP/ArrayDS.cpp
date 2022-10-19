4ff192526054039cd487a2db69e873963a5eff42
Abhishek <garg75468@gmail.com>
2022-10-19 20:48:41 +0530

    array written in c++

---
+++ src/CPP/ArrayDS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// array declaration by specifying size
+	int arr1[10];
+	
+	
+	// declare an array of user specified size
+	int n = 10;
+	int arr2[n];
+	
+	return 0;
+}
+
+
