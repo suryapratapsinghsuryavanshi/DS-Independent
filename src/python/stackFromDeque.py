3d777d10c62d3d5064c596db23613f581d73bf6c
Sachin Shah <sachinshah1999@gmail.com>
2023-10-20 00:40:14 +0530

    add stack in python using deque

---
+++ src/python/stackFromDeque.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+stack = deque()
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial stack:')
+print(stack)
+
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
+
